66fbace6f6e02f340b080837f7dcf7b88c31c4eb
yohori <y.ohori0923@gmail.com>
2022-09-12 16:11:45 +0900

    first upload

--- OpenFile.java
+++ OpenFile.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class OpenFile{
     public void open(){
-        
+
         Map<String,String> data = new HashMap<>();
-        
+
         while(true){
             System.out.println("ファイルのパスを入力してください");
             Scanner scanner = new Scanner(System.in);
@@ -17,14 +17,9 @@ public class OpenFile{
                 while(line != null){
                     String[] st1 = line.split(",");
                     System.out.println(st1[0]);
-     
+
     //ハッシュが定義されていれば加算
-                    if(data.containskey(st1[0])){
-                        data.put = (st1[0],data.get(st1[0]) + st1[1]);
-                    }else{
-                        data.put = (st1[0], st1[1]);
-                    }
-                    
+
                     line = br.readLine();
                 }
                 br.close();
